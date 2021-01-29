323be446f7728bd0c6d3e2297b435192f12b3faa
Mickael GOETZ <contact@mickael-goetz.com>
2021-01-29 23:11:29 +0100

    feat: match file path as well

--- jetbrains/project_parser.py
+++ jetbrains/project_parser.py
@@ -21,9 +21,9 @@ class RecentProjectsParser():
 
         root = ET.parse(file_path).getroot()
 
-        recent_projects = root.findall(
+        recent_projects = root.findall(  # recent projects in products version 2020.2 and below
             './/component[@name="RecentProjectsManager"][1]/option[@name="recentPaths"]/list/option'
-        ) + root.findall(
+        ) + root.findall(  # recent projects in products version 2020.2 and below
             './/component[@name="RecentDirectoryProjectsManager"][1]/option[@name="recentPaths"]/list/option'
         ) + root.findall(  # projects in groups in products version 2020.3+
             './/component[@name="RecentProjectsManager"][1]/option[@name="groups"]/list/ProjectGroup/option'
@@ -32,35 +32,36 @@ class RecentProjectsParser():
             './/component[@name="RecentProjectsManager"][1]/option[@name="additionalInfo"]/map/entry'
         )
 
+        home = os.path.expanduser('~')
+        query = query.lower() if query else query
+
         result = []
         already_matched = []
         for project in recent_projects:
-            project_title = ''
-            project_path = (project.attrib['value' if 'value' in project.attrib else 'key']).replace(
-                '$USER_HOME$', os.path.expanduser('~'))
-            name_file = project_path + '/.idea/.name'
+            title = ''
+            path = (project.attrib['value' if 'value' in project.attrib else 'key']).replace('$USER_HOME$', home)
+            name_file = path + '/.idea/.name'
 
             if os.path.exists(name_file):
                 with open(name_file, 'r') as file:
-                    project_title = file.read().replace('\n', '')
+                    title = file.read().replace('\n', '')
 
-            project_name = os.path.basename(project_path)
-            icons = glob.glob(os.path.join(project_path, '.idea', 'icon.*'))
+            name = os.path.basename(path)
+            icons = glob.glob(os.path.join(path, '.idea', 'icon.*'))
 
-            if query and query.lower() not in project_name.lower(
-            ) and query.lower() not in project_title.lower():
+            if query and query not in name.lower() and query not in title.lower() and query not in path:
                 continue
 
             # prevent duplicate results, because from version 2020.3, a project can appear more than once in the XML
             # (in the option[@name="groups"] section and in the option[@name="additionalInfo"] section)
-            if project_path in already_matched:
+            if path in already_matched:
                 continue
 
-            already_matched.append(project_path)
+            already_matched.append(path)
 
             result.append({
-                'name': project_title or project_name,
-                'path': project_path,
+                'name': title or name,
+                'path': path,
                 'icon': icons[0] if len(icons) > 0 else None
             })
 
